5962390daa8f39505573b77fd4045ecc2a7554cc
gunphasin <phasin@wongnai.com>
2020-05-19 16:01:31 +0700

    add file UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [file, setFile] = useState(null)
+
+  const onChange = useCallback((e) => {
+    const file = e.target.files[0]
+    setFile(file)
+  }, [])
+
+  const onClick = useCallback(() => {
+    if (file) {
+      console.log(file, 'upload')
+      const formData = new FormData()
+      formData.append('file', file)
+      axios
+      .post('http://localhost:3000/upload-file', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(() => {
+        console.log('SUCCESS!!')
+      })
+      .catch(err => {
+        console.log(err, 'FAILURE!!')
+      })
+    }
+  }, [file])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="file" onChange={onChange} />
+        <button onClick={onClick}>Upload</button>
       </header>
     </div>
   );
